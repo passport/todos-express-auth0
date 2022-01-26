fbcacb3acf52bb05ba6d0401b3d161b39d23248a
Jared Hanson <jaredhanson@gmail.com>
2022-01-26 09:19:21 -0800

    Log user out of Auth0 session.

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+var qs = require('querystring');
 var express = require('express');
 var passport = require('passport');
 var OpenIDConnectStrategy = require('passport-openidconnect');
@@ -41,7 +42,11 @@ router.get('/oauth2/redirect', passport.authenticate('openidconnect', {
 
 router.post('/logout', function(req, res, next) {
   req.logout();
-  res.redirect('/');
+  var params = {
+    client_id: process.env['AUTH0_CLIENT_ID'],
+    returnTo: 'http://localhost:3000/'
+  };
+  res.redirect('https://' + process.env['AUTH0_DOMAIN'] + '/v2/logout?' + qs.stringify(params));
 });
 
 module.exports = router;
